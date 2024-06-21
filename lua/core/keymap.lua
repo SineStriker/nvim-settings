0478734919a1c03cf585c6127a4eab6cc3d6277d
SineStriker <trueful@163.com>
2024-06-22 06:06:54 +0800

    Update keymap

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -1,15 +1,19 @@
 local keyset = vim.keymap.set   -- 快捷键
+local opt = { noremap = true, silent = true }
 
 -- https://unix.stackexchange.com/questions/11402/why-does-esc-move-the-cursor-back-in-vim
 -- 防止按 Esc 回到普通模式后光标左移
 vim.cmd [[ au InsertLeave * call cursor([getpos('.')[1], getpos('.')[2]+1]) ]]
 
 -- 设置 Tab 与 Shift+Tab 为选中行首部增加或删除制表符
-keyset('v', '<Tab>', '>gv', { noremap = true, silent = true })
-keyset('v', '<S-Tab>', '<gv', { noremap = true, silent = true })
+keyset('v', '<Tab>', '>gv', opt)
+keyset('v', '<S-Tab>', '<gv', opt)
 
--- 保存
-keyset('i', '<c-s>', '<Esc>:w<CR>i')
+-- Ctrl+S 保存
+keyset('i', '<c-s>', '<Esc>:w<CR>i', opt)
+
+-- Ctrl+Q 关闭
+keyset('n', '<c-q>', ':q<CR>', opt)
 
 -- 撤销/重做
 -- local function safe_undo()
